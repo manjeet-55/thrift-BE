f4b0e764093686b53220620bc037fa5f65f716aa
manjeet-55 <manjeet.agarwal@fiftyfivetech.io>
2024-10-17 11:12:48 +0530

    nestJs initial setup

--- src/router/auth.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-import { register } from "../controllers/auth";
-import router from ".";
-
-export default (router: express.Router) => {
-  router.post("/auth/register", register);
-};
